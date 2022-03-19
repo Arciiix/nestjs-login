72f2d53bd0756c22c48478413fcd9030bfca4f16
Arciiix <artur.nowak100@gmail.com>
2022-03-19 22:23:50 +0100

    Add prisma

---
+++ src/app/app.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AppService {
+  constructor(private prisma: PrismaService) {}
+  getHello(): { message: string } {
+    return { message: "Hello, world!" };
+  }
+}
